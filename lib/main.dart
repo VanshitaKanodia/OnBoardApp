5dfe760ae0d5f8962394aaac9385287939cf345c
Bluetooth_scanning <vanshitakanodia00@gmail.com>
2024-04-18 18:04:40 +0530

    bluetooth update

--- lib/main.dart
+++ lib/main.dart
@@ -17,28 +17,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//
-//
-// import 'package:flutter/material.dart';
-// import 'package:iwayplus_bluetooth/views/home_page.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const HomePage(),
-//     );
-//   }
-// }
