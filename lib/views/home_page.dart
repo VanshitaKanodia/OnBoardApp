d09c075a6513b9b63bf20d14a153efc0e9d061f1
Bluetooth_scanning <vanshitakanodia00@gmail.com>
2024-04-19 18:11:15 +0530

    ui fix

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,44 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:iwayplus_bluetooth/controller/bluetooth_controller.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<BluetoothDevice> _devices = [];
-
-  Future<void> _scanDevices() async {
-    List<BluetoothDevice> devices = await BluetoothController().scanDevices();
-    setState(() {
-      _devices = devices;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bluetooth Devices'),
-        bottom: PreferredSize(
-            preferredSize: const Size(100, 40),
-            child: ElevatedButton(
-                onPressed: _scanDevices, child: const Text('Scan'))),
-      ),
-      body: ListView.builder(
-        itemCount: _devices.length,
-        itemBuilder: (context, index) {
-          BluetoothDevice device = _devices[index];
-          return ListTile(
-            title: Text(device.name),
-            subtitle: Text(device.id.toString()),
-          );
-        },
-      ),
-    );
+    return const Placeholder();
   }
 }
