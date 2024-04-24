4ec04a6b5c08b1690fe89175f3887fa084df8c5e
VanshitaKanodia <vanshitakanodia00@gmail.com>
2024-04-24 18:19:41 +0530

    vibration init

--- lib/views/auto_query.dart
+++ lib/views/auto_query.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:permission_handler/permission_handler.dart';
