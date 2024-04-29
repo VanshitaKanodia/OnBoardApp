75bb6e356e44335cbbafbbdbbeee6c8ed88e2833
VanshitaKanodia <vanshitakanodia00@gmail.com>
2024-04-30 02:06:04 +0530

    otp field changes

--- lib/auth_page/otp_screen.dart
+++ lib/auth_page/otp_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:otp_text_field/otp_text_field.dart';
 import 'package:otp_text_field/style.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpPage extends StatefulWidget {
   const OtpPage({super.key});
@@ -11,51 +12,62 @@ class OtpPage extends StatefulWidget {
 
 class _OtpPageState extends State<OtpPage> {
   OtpFieldController otpController = OtpFieldController();
+  var code = "";
+
+  // Assume expectedOTP is the OTP received from the server
+  String expectedOTP = "123456"; // Example OTP, replace it with your actual OTP
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.arrow_back_ios),
-          actions: [
-            TextButton(onPressed: () {}, child: const Text('Change Number'))
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Text(
-                'Enter authentication code',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color(0xFF090A0A),
-                  fontSize: 24,
-                  fontFamily: 'Open Sans',
-                  fontWeight: FontWeight.w700,
-                  height: 0.06,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: const Icon(Icons.arrow_back_ios),
+            actions: [
+              TextButton(onPressed: () {}, child: const Text('Change Number'))
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(
+                  'Enter authentication code',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFF090A0A),
+                    fontSize: 24,
+                    fontFamily: 'Open Sans',
+                    fontWeight: FontWeight.w700,
+                    height: 0.06,
+                  ),
                 ),
-              ),
-              SizedBox(height: 20,),
-              const Text(
-                'Enter the 4-digit code that we have sent via the phone number +91 1234567890',
-                style: TextStyle(
-                  color: Color(0xFF090A0A),
-                  fontSize: 16,
-                  fontFamily: 'Open Sans',
-                  fontWeight: FontWeight.w400,
+                SizedBox(height: 20,),
+                const Text(
+                  'Enter the 4-digit code that we have sent via the phone number +91 1234567890',
+                  style: TextStyle(
+                    color: Color(0xFF090A0A),
+                    fontSize: 16,
+                    fontFamily: 'Open Sans',
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(vertical: 40.0),
-                child: OTPTextField(
+                Container(
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  padding: EdgeInsets.symmetric(vertical: 40.0),
+                  child: OTPTextField(
                     controller: otpController,
-                    length: 5,
+                    length: 6,
+                    // Set the length to 6 digits
                     textFieldAlignment: MainAxisAlignment.spaceAround,
-                    fieldWidth: 20,
+                    fieldWidth: 30,
+                    // Increase the field width to accommodate 6 digits
                     fieldStyle: FieldStyle.underline,
                     style: TextStyle(fontSize: 17),
                     onChanged: (pin) {
@@ -63,8 +75,10 @@ class _OtpPageState extends State<OtpPage> {
                     },
                     onCompleted: (pin) {
                       print("Completed: " + pin);
-                    }),
-              ),
+                    },
+                  ),
+
+                ),
                 // OtpTextField(
                 //   // borderRadius: const BorderRadius.all(Radius.circular(280)),
                 //   fieldWidth: 55,
@@ -76,52 +90,64 @@ class _OtpPageState extends State<OtpPage> {
                 //     verificationCode = '';
                 //   }, // end onSubmit
                 // ),
-              GestureDetector(
-                  onTap: () {},
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 327,
-                        height: 48,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        decoration: ShapeDecoration(
-                          color: const Color(0xFF6A4DFF),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(48),
+                GestureDetector(
+                    onTap: () {
+                        String enteredOTP = otpController.toString();
+                        if (enteredOTP == expectedOTP) {
+                          // OTP verification successful
+                          print("OTP verification successful");
+                          // Proceed with login process
+                          // Call your login function here or navigate to the next screen
+                        } else {
+                          // OTP verification failed
+                          print("OTP verification failed");
+                          // Show error message or handle accordingly
+                        }
+                    },
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 327,
+                          height: 48,
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          decoration: ShapeDecoration(
+                            color: const Color(0xFF6A4DFF),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(48),
+                            ),
                           ),
-                        ),
-                        child: const Center(
-                          child: Text(
-                            'Continue',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontFamily: 'Open Sans',
-                              fontWeight: FontWeight.w600,
-                              height: 0.05,
+                          child: const Center(
+                            child: Text(
+                              'Continue',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontFamily: 'Open Sans',
+                                fontWeight: FontWeight.w600,
+                                height: 0.05,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Resend Code',
-                            style: TextStyle(
-                              color: Color(0xFF2563EB),
-                              fontSize: 14,
-                              fontFamily: 'Open Sans',
-                              fontWeight: FontWeight.w600,
-                              height: 0.11,
-                            ),
-                          )),
-                    ],
-                  )),
-            ],
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Resend Code',
+                              style: TextStyle(
+                                color: Color(0xFF2563EB),
+                                fontSize: 14,
+                                fontFamily: 'Open Sans',
+                                fontWeight: FontWeight.w600,
+                                height: 0.11,
+                              ),
+                            )),
+                      ],
+                    )),
+              ],
+            ),
           ),
-        ),
-      )
+        )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:iwayplus_bluetooth/auth_page/Init_page.dart';
-import 'package:iwayplus_bluetooth/auth_page/otp_screen.dart';
 import 'package:iwayplus_bluetooth/auth_page/logInPage.dart';
+import 'package:iwayplus_bluetooth/auth_page/otp_screen.dart';
 import 'package:iwayplus_bluetooth/auth_page/signup_page.dart';
-import 'package:iwayplus_bluetooth/views/auto_query.dart';
 import 'package:iwayplus_bluetooth/views/home_page.dart';
-import 'package:iwayplus_bluetooth/views/myRoute_page.dart';
-import 'package:iwayplus_bluetooth/views/query.dart';
 
 
 void main() {
@@ -27,14 +24,10 @@ class MyApp extends StatelessWidget {
         initialRoute: 'otp_page',
         routes: {
           'init_page' : (context) => InitPage(),
-          'logIn_page' : (context) => LogInPage(),
+          'signIn_page' : (context) => LogInPage(),
           'signUp_page' : (context) => SignupPage(),
           'otp_page' : (context) => OtpPage(),
           'home_page': (context) => HomePage(),
-          'query_page' : (context) => QueryPage(),
-          'auto_query_page' : (context) => AutoQueryPage(),
-          'myRoute_page': (context) => MyRoutes(),
         },
-        home: LogInPage(),
       );
 }
